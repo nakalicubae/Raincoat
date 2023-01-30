ec54557bcf9229e1b1be4aa091375d42458b3fa7
okxooxoo <okxooxoo@naver.com>
2023-01-30 14:17:13 +0900

    SignUp Input

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignUp from "./screens/SignUp";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='SignUp' screenOptions = {{ headerShown: false }}>
+        <Stack.Screen name='SignUp' component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/SignUp.js
@@ -0,0 +1,107 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+
+export default function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <View style={styles.email}>
+        <Text style={styles.emailText}>이메일</Text>
+        <TextInput
+          placeholder="이메일을 입력하세요."
+          style={styles.emailInput}
+        />
+        <TouchableOpacity style={styles.authReq}>
+          <Text style={styles.authReqText}>인증 요청</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.auth}>
+        <TextInput style={styles.authCode}/>
+        <Text style={styles.authTime}>4:39</Text>
+        <TouchableOpacity style={styles.authSend}>
+          <Text style={styles.authSendText}>인증 확인</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+
+  email: {
+    flexDirection: "row",
+    alignItems: "center",
+    //backgroundColor: "blue"
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+
+  emailText: {
+    //fontSize: 18,
+    //fontWeight: "600",
+  },
+
+  emailInput: {
+    flex: 1,
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginHorizontal: 10,
+  },
+
+  authReq: {
+    backgroundColor: "black",
+    borderRadius: 10,
+    padding: 10,
+  },
+
+  authReqText: {
+    color: "white",
+  },
+
+  auth: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+
+  authCode: {
+    flex: 1,
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  authTime: {
+    marginHorizontal: 10,
+  },
+
+  authSend: {
+    backgroundColor: "black",
+    borderRadius: 10,
+    //fontSize: 18,
+    padding: 10,
+  },
+
+  authSendText: {
+    color: "white",
+  }
+});
