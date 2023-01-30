c6f2f61a31ee33d2f9d6484772a42b31146e981c
okxooxoo <okxooxoo@naver.com>
2023-01-30 14:59:52 +0900

    ADD: pw / nickname Input

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -25,12 +25,40 @@ export default function SignUp({ navigation }) {
       </View>
 
       <View style={styles.auth}>
-        <TextInput style={styles.authCode}/>
+        <TextInput style={styles.authCode} />
         <Text style={styles.authTime}>4:39</Text>
         <TouchableOpacity style={styles.authSend}>
           <Text style={styles.authSendText}>인증 확인</Text>
         </TouchableOpacity>
       </View>
+
+      <View style={styles.pw}>
+        <Text style={styles.pwText}>비밀번호</Text>
+        <TextInput
+          placeholder="비밀번호를 입력하세요."
+          style={styles.pwInput}
+        />
+      </View>
+
+      <View style={styles.pw}>
+        <Text style={styles.pwText}>비밀번호 확인</Text>
+        <TextInput
+          placeholder="비밀번호를 입력하세요."
+          style={styles.pwInput}
+        />
+      </View>
+
+      <View style={styles.nickname}>
+        <Text style={styles.nicknameText}>닉네임</Text>
+        <TextInput
+          placeholder="닉네임을 입력하세요."
+          style={styles.nicknameInput}
+        />
+        <TouchableOpacity style={styles.authReq}>
+          <Text style={styles.authReqText}>중복 확인</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -103,5 +131,37 @@ const styles = StyleSheet.create({
 
   authSendText: {
     color: "white",
+  },
+
+  pw: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+
+  pwInput: {
+    flex: 1,
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginLeft: 10,
+  },
+
+  nickname: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+
+  nicknameInput: {
+    flex: 1,
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginHorizontal: 10,
   }
 });
