2d95e930c522afa738b1ad9380a4e1c14da477f7
okxooxoo <okxooxoo@naver.com>
2023-02-02 21:32:58 +0900

    Add hr / signup btn

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -14,51 +14,46 @@ export default function SignUp({ navigation }) {
       <StatusBar style="auto" />
 
       <View style={styles.email}>
-        <Text style={styles.emailText}>이메일</Text>
-        <TextInput
-          placeholder="이메일을 입력하세요."
-          style={styles.emailInput}
-        />
-        <TouchableOpacity style={styles.authReq}>
-          <Text style={styles.authReqText}>인증 요청</Text>
+        <Text style={styles.text}>이메일</Text>
+        <TextInput placeholder="이메일을 입력하세요." style={styles.input} />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.text}>인증 요청</Text>
         </TouchableOpacity>
       </View>
 
       <View style={styles.auth}>
-        <TextInput style={styles.authCode} />
-        <Text style={styles.authTime}>4:39</Text>
-        <TouchableOpacity style={styles.authSend}>
-          <Text style={styles.authSendText}>인증 확인</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.text}>4:39</Text>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.text}>인증 확인</Text>
         </TouchableOpacity>
       </View>
 
+      <View style={styles.hr} />
+
       <View style={styles.pw}>
-        <Text style={styles.pwText}>비밀번호</Text>
-        <TextInput
-          placeholder="비밀번호를 입력하세요."
-          style={styles.pwInput}
-        />
+        <Text style={styles.text}>비밀번호</Text>
+        <TextInput placeholder="비밀번호를 입력하세요." style={styles.input} />
       </View>
 
       <View style={styles.pw}>
-        <Text style={styles.pwText}>비밀번호 확인</Text>
-        <TextInput
-          placeholder="비밀번호를 입력하세요."
-          style={styles.pwInput}
-        />
+        <Text style={styles.text}>비밀번호 확인</Text>
+        <TextInput placeholder="비밀번호를 입력하세요." style={styles.input} />
       </View>
 
-      <View style={styles.nickname}>
-        <Text style={styles.nicknameText}>닉네임</Text>
-        <TextInput
-          placeholder="닉네임을 입력하세요."
-          style={styles.nicknameInput}
-        />
-        <TouchableOpacity style={styles.authReq}>
-          <Text style={styles.authReqText}>중복 확인</Text>
+      <View style={styles.hr} />
+
+      <View style={styles.name}>
+        <Text style={styles.text}>닉네임</Text>
+        <TextInput placeholder="닉네임을 입력하세요." style={styles.input} />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.text}>중복 확인</Text>
         </TouchableOpacity>
       </View>
 
+      <TouchableOpacity style={styles.signUp}>
+        <Text style={styles.text}>회원가입</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -75,93 +70,66 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     //backgroundColor: "blue"
-    marginHorizontal: 20,
+    marginHorizontal: 15,
     marginVertical: 5,
-  },
-
-  emailText: {
-    //fontSize: 18,
-    //fontWeight: "600",
-  },
-
-  emailInput: {
-    flex: 1,
-    borderRadius: 10,
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-    marginHorizontal: 10,
-  },
-
-  authReq: {
-    backgroundColor: "black",
-    borderRadius: 10,
-    padding: 10,
-  },
-
-  authReqText: {
-    color: "white",
   },
 
   auth: {
     flexDirection: "row",
     alignItems: "center",
-    marginHorizontal: 20,
+    marginHorizontal: 15,
     marginVertical: 5,
-  },
-
-  authCode: {
-    flex: 1,
-    borderRadius: 10,
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-  },
-
-  authTime: {
-    marginHorizontal: 10,
-  },
-
-  authSend: {
-    backgroundColor: "black",
-    borderRadius: 10,
-    //fontSize: 18,
-    padding: 10,
-  },
-
-  authSendText: {
-    color: "white",
   },
 
   pw: {
     alignItems: "center",
     flexDirection: "row",
-    marginHorizontal: 20,
+    marginHorizontal: 15,
     marginVertical: 5,
   },
 
-  pwInput: {
-    flex: 1,
-    borderRadius: 10,
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-    marginLeft: 10,
-  },
-
-  nickname: {
+  name: {
     flexDirection: "row",
     alignItems: "center",
-    marginHorizontal: 20,
+    marginHorizontal: 15,
     marginVertical: 5,
   },
 
-  nicknameInput: {
+  input: {
     flex: 1,
     borderRadius: 10,
-    borderColor: "black",
+    borderColor: "gold",
     borderWidth: 1,
     padding: 10,
-    marginHorizontal: 10,
-  }
+    marginHorizontal: 5,
+  },
+
+  btn: {
+    backgroundColor: "gold",
+    borderColor: "gold",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginHorizontal: 5,
+  },
+
+  text: {
+    marginHorizontal: 5,
+  },
+
+  signUp: {
+    backgroundColor: "gold",
+    borderColor: "gold",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 20,
+  },
+
+  hr: {
+    width: "90%",
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginVertical: 10,
+  },
 });
