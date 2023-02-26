d2c502332af543d6d9bae53865a1dedf68542427
seongeun <kse011010@gmail.com>
2023-02-26 22:08:47 +0900

    Update in Main.js

--- screens/Main.js
+++ screens/Main.js
@@ -3,34 +3,60 @@ import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
-  TextInput,
   View,
   ScrollView,
   TouchableOpacity,
 } from "react-native";
-import { Divider } from 'react-native-elements'
 import { MaterialCommunityIcons, Feather, Ionicons } from '@expo/vector-icons';
- import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
 
 
 
 export default function Main({ navigation }) {
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
+
       <View style={styles.menuBar}>
-        <MaterialCommunityIcons name="microsoft-xbox-controller-menu" size={30} color="black" />
+        <Ionicons name="person-circle" size={40} color="black" />
+        <MaterialCommunityIcons name="microsoft-xbox-controller-menu" size={40} color="black" />
       </View>
 
       <View style={styles.list}>
         <ScrollView horizontal pagingEnabled>
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
-          <Feather name="book" size={50} color="black" />
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>A</Text>
+          </View>
+          
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>B</Text>
+          </View>
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>C</Text>
+          </View>
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>D</Text>
+          </View>
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>E</Text>
+          </View>
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>F</Text>
+          </View>
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>G</Text>
+          </View>
+          <View style={styles.diary} >
+          <Feather name="book" size={40} color="black"/>
+            <Text>H</Text>
+          </View>
         </ScrollView>
         <MaterialCommunityIcons name="arrow-right-drop-circle" size={30} color="black" />
       </View>
@@ -39,15 +65,23 @@ export default function Main({ navigation }) {
         <Calendar style={styles.calendar} />
       </View>
 
-      <View>
-        <Text style={styles.today}>오늘의 우비들</Text>
-        <View style={styles.members}>
-          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
-          <Ionicons style={styles.person}name="person-outline" size={50} color="black" />
-          <Ionicons style={styles.person}name="person-outline" size={50} color="black" />
-          <Ionicons style={styles.person}name="person-outline" size={50} color="black" />
+      <Text style={styles.today}>오늘의 우비들</Text>
+
+      <View style={styles.members}>
+        <View View style={styles.member}>
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+        </View>
+        <View View style={styles.member}>
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
         </View>
       </View>
+      
 
       <View>
         <TouchableOpacity
@@ -56,46 +90,49 @@ export default function Main({ navigation }) {
           <Text>우비쓰기</Text>
         </TouchableOpacity>
       </View>
-      
     </View>
-
-    
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
   },
 
   menuBar: {
+    flexDirection: 'row',
     alignSelf: 'flex-end',
     marginHorizontal: 20,
     marginVertical: 20,
   },
 
   list: {
-    flexDirection: "row",
-    alignSelf: 'flex-start',
+    flexDirection: 'row',
     marginHorizontal: 20,
-    marginVertical: 10,
     marginBottom: 30,
+    paddingRight: 60,
   },
+
 
   today: {
     alignSelf: 'flex-start',
-    marginVertical: 30,
-
+    marginHorizontal: 30,
+    marginTop: 30,
+    fontSize: 20,
+    letterSpacing: 3
   },
 
   members: {
-    flexDirection: "row",
-    marginHorizontal: 20,
-    marginVertical: 10,
+    marginTop: 10
+  },
+
+  member: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginVertical: 5,
   },
 
   person: {
@@ -103,11 +140,17 @@ const styles = StyleSheet.create({
   },
 
   write: {
-    backgroundColor: "gold",
-    borderColor: "gold",
+    backgroundColor: 'gold',
+    borderColor: 'gold',
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
     marginTop: 20,
   },
+
+  diary: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
 });
