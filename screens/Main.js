24b965dbafc4e698823887a0eb1b783c4899e171
seongeun <kse011010@gmail.com>
2023-03-23 13:48:03 +0900

    Create Modal and Update MainPage

--- screens/Main.js
+++ screens/Main.js
@@ -6,60 +6,104 @@ import {
   View,
   ScrollView,
   TouchableOpacity,
+  Modal,
+  Pressable,
+  Alert
+
 } from "react-native";
 import { MaterialCommunityIcons, Feather, Ionicons } from '@expo/vector-icons';
 import { Calendar } from 'react-native-calendars';
 
 
-
 export default function Main({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <View style={styles.menuBar}>
-        <Ionicons name="person-circle" size={40} color="black" />
-        <MaterialCommunityIcons name="microsoft-xbox-controller-menu" size={40} color="black" />
-      </View>
-
-      <View style={styles.list}>
+      <View style={styles.header}>
+        <View style={styles.list}>
         <ScrollView horizontal pagingEnabled>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>A</Text>
-          </View>
-          
-          <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>B</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>A</Text>
           </View>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>C</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>B</Text>
           </View>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>D</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>C</Text>
           </View>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>E</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>D</Text>
           </View>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>F</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>E</Text>
           </View>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>G</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>F</Text>
           </View>
           <View style={styles.diary} >
-          <Feather name="book" size={40} color="black"/>
-            <Text>H</Text>
+            <Feather name="book" size={40} color="black"/>
+              <Text>G</Text>
+          </View>
+          <View style={styles.diary} >
+            <Feather name="book" size={40} color="black"/>
+              <Text>H</Text>
           </View>
         </ScrollView>
-        <MaterialCommunityIcons name="arrow-right-drop-circle" size={30} color="black" />
+          <MaterialCommunityIcons name="arrow-right-drop-circle" size={30} color="black" />
+        </View>
+
+        <View style={styles.menuBar}>
+          <Ionicons name="person-circle" size={40} color="black" />
+          {/* <MaterialCommunityIcons name="microsoft-xbox-controller-menu" size={40} color="black" /> */}
+          <Pressable
+            onPress={() => setModalVisible(true)}>
+            {/* <Text style={styles.textStyle}>Show Modal</Text> */}
+            <MaterialCommunityIcons name="microsoft-xbox-controller-menu" size={40} color="black" />
+          </Pressable>
+        </View>
+
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>친구추가</Text>
+                <Text style={styles.modalText}>모임생성</Text>
+                {/* <Text style={styles.modalText}>관리자/멤버 설정</Text>
+                <Text style={styles.modalText}>모임 이미지 설정</Text>
+                <Text style={styles.modalText}>모임 이름 설정</Text>  이것들은 관리자 영역*/} 
+                <Text style={styles.modalText}>고객센터</Text>
+
+                <Pressable
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <MaterialCommunityIcons name="microsoft-xbox-controller-menu" size={40} color="black" />
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
       </View>
+
+      <View style={styles.current}>
+        <Feather style={styles.diary} name="book" size={55} color="black"/>
+        <Text style={styles.currentDiaryName}>A</Text>
+      </View>
+
+      
 
       <View style={styles.calendarContainer}>
         <Calendar style={styles.calendar} />
@@ -68,21 +112,18 @@ export default function Main({ navigation }) {
       <Text style={styles.today}>오늘의 우비들</Text>
 
       <View style={styles.members}>
-        <View View style={styles.member}>
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-        </View>
-        <View View style={styles.member}>
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-          <Ionicons style={styles.person} name="person-outline" size={40} color="black" />
-        </View>
+        <ScrollView horizontal pagingEnabled>
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+          <Ionicons style={styles.person} name="person-outline" size={50} color="black" />
+        </ScrollView>
       </View>
-      
-
+  
       <View>
         <TouchableOpacity
         onPress={() => navigation.navigate("Write")}
@@ -102,18 +143,26 @@ const styles = StyleSheet.create({
     backgroundColor: 'white'
   },
 
+  header: {
+    flexDirection: 'row',
+    alignContent: 'space-between',
+    marginHorizontal: 20,
+    marginBottom: 20
+  },
+
+  calendarContainer: {
+    width: 350
+  },
+
   menuBar: {
     flexDirection: 'row',
-    alignSelf: 'flex-end',
-    marginHorizontal: 20,
-    marginVertical: 20,
+
   },
 
   list: {
+    flex: 1,
     flexDirection: 'row',
-    marginHorizontal: 20,
-    marginBottom: 30,
-    paddingRight: 60,
+    marginRight: 60
   },
 
 
@@ -125,14 +174,31 @@ const styles = StyleSheet.create({
     letterSpacing: 3
   },
 
-  members: {
-    marginTop: 10
+  current: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    marginHorizontal: 20,
+    marginBottom: 20
   },
 
-  member: {
+  currentDiaryName: {
+    marginLeft: 10,
+    fontSize: 30,
+      textAlignVertical: 'center',
+      ...Platform.select({
+          ios: {
+              lineHeight: 60 // as same as height
+          },
+          android: {}
+        })
+  },
+
+
+  members: {
     flexDirection: 'row',
-    marginHorizontal: 10,
-    marginVertical: 5,
+    margin: 30,
+    marginLeft: 50,
+    marginRight: 50
   },
 
   person: {
@@ -153,4 +219,37 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 
+
+  centeredView: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    // marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 40,
+    textAlign: 'center',
+    fontSize: 20
+  },
 });
